189313edd25680f20c6c869e8add1c34033462e7
Nutchapol-Rpch <97792035+Smiffeed@users.noreply.github.com>
2026-02-21 15:28:39 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,13 @@
+import sys
+print("arguments", sys.argv)
+
+month = int(sys.argv[1])
+print(f"Running pipeline for day {month}")
+import pandas as pd
+
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df['month']=month
+print(df.head())
+
+df.to_parquet(f"output_{month}.parquet")
+print(f'hello pipeline, month={month}')
